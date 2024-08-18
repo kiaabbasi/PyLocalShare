da1191dc3e1949f813e8e905ded1a62d8651a60f
kia <behkia007@gmail.com>
2024-08-19 01:10:11 +0330

    we have udp broad cast for self detect on local networks

--- fileTransferPortocol.py
+++ fileTransferPortocol.py
@@ -1,5 +1,8 @@
 import socket
-from time import sleep
+from time import sleep,time
+import os
+import configs
+from _thread import start_new_thread
 def send_file_tcp (file_addres , tcp_socket :socket.socket,file_seek=0,chunk_size=4096):
     with open (file_addres,mode="rb") as f :
         f.seek(0, 2) 
@@ -52,4 +55,97 @@ def recv_file_tcp(save_as, tcp_socket: socket.socket, buffer_size=4096):
             f.write(chunk)
             bytes_received += len(chunk)
     
+
+
+class Divice:
+    # address is ip addres and tcp port
+    def __init__(self,version,address,name=None,platform=None) -> None:
+        #self.name=name
+        self.version=version
+        self.address=address
+        if name==None:
+            self.name=socket.gethostbyaddr(address[0])[0]
+        else:
+            self.name=name
+        self.platform=platform
+
+    def update():#TODO update the information abut this Divece
+        pass
+
+    def __eq__(self, other) -> bool:
+        return self.name==other.name and self.address == other.address
+    def __str__(self) -> str:
+        if self.name:
+            return f"{self.name}-{self.version}"
+        else:
+            return f"{self.address[0]}-{self.version}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+class Udp_Brodcast :
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.__start_lock=False
+        self.is_in_progress=False
+
+
+    def detect_divces(timer=6)->list:
+        divces=[]
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        server_address = ('', configs.network.LOCAL_UDP_BRODCAST_PORT)
+        sock.bind(server_address)
+        target_time=time()+timer
+        while time()<=target_time:
+            data, address = sock.recvfrom(1024)
+        
+            data =data.decode()
+            if "pylocalshare" in data:
+                data = data.split(" ")
+                d = Divice(data[data.index("from")+1],(address[0],data[data.index("on")+1]))
+                if d not in divces : 
+                    divces.append(d)
+
+        return divces
+
+    def start(self):
+        def main_loop():
+            try :
+                while self.__start_lock:
+                    self.is_in_progress = True
+                    message = f"pylocalshare from {configs.network.LOCAL_UDP_BRODCAST_VERSOINS} on {configs.network.LOCAL_TCP_PORT}"
+                    broadcast_address = ('<broadcast>',configs.network.LOCAL_UDP_BRODCAST_PORT)
+                    
+                    self.sock.sendto(message.encode(), broadcast_address)
+                    sleep(5)
+            finally :
+                self.is_in_progress = False
+        if not self.is_in_progress:
+            self.__start_lock=True
+            start_new_thread(main_loop,tuple([]))
+
+
+    def stop(self):
+        self.__start_lock = False
     
+    def __del__(self) -> None:
+        self.sock.close()
+
+
+if __name__ == "__main__":
+    udp = Udp_Brodcast()
+    udp.start()
+    sleep(1)
+    print(Udp_Brodcast.detect_divces())
+    while True :
+        cmd = input(">>>")
+        print(udp.is_in_progress)
+
+
+        if  cmd == "y":
+            udp.stop()
+
+        if  cmd == "q":
+            udp.stop()
+            del udp
+            break
